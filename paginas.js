b7ee5de76fc8b87fb2e09a5363148e1c46d354dd
RafaelaVellardo <162139416+RafaelaaVellardo@users.noreply.github.com>
2025-04-21 22:11:50 -0300

    Add files via upload

---
+++ paginas.js
@@ -0,0 +1,21 @@
+function linguagens(){
+    window.location.href="paginaLing.html"
+}
+function humanas(){
+    window.location.href="paginaHum.html"
+}
+function natureza(){
+    window.location.href="paginaNat.html"
+}
+function matematica(){
+    window.location.href="paginaMat.html"
+}
+function tecnico(){
+    window.location.href="paginaTec.html"
+}
+function outros(){
+    window.location.href="paginaOutr.html"
+}
+function inicio(){
+    window.location.href="inicio.html"
+}
